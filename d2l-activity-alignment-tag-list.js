7d67858217a16821e97c26519a29dec679a8cfe8
iFun <bill.guocheng@gmail.com>
2019-03-18 11:27:58 -0400

    change alt text to browse outcome

--- d2l-activity-alignment-tag-list.js
+++ d2l-activity-alignment-tag-list.js
@@ -60,7 +60,7 @@ class ActivityAlignmentTagList extends mixinBehaviors([
 				<template is="dom-if" if="[[_canUpdate(entity,readOnly)]]">
 					<d2l-button-icon
 						icon="d2l-tier1:add"
-						text="[[localize('removeAlignment')]]"
+						text="[[localize('browseOutcome')]]"
 						style$="[[_iconStyle]]"
 						on-click="_updateAlignments"
 					></d2l-button-icon>
