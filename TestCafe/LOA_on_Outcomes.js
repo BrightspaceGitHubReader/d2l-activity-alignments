9597f067ba1baed4592502403b46003ed4d5dbd0
Dave <davewesterveld@gmail.com>
2019-05-03 14:56:10 -0400

    Add Tests (#80)
    * refactor and add another test
    
    * remove unused code
    
    * remove unused code
    
    * ignore line

--- TestCafe/LOA_on_Outcomes.js
+++ TestCafe/LOA_on_Outcomes.js
@@ -1,20 +1,32 @@
 import { Selector } from 'testcafe';
 
-fixture `Select`;// eslint-disable-line no-undef
+fixture `Select`// eslint-disable-line no-undef
+	.page `http://127.0.0.1:8081/components/d2l-activity-alignments/demo/`
+	.beforeEach(async t => {
+		t.ctx.checkBox = Selector(() => {
+			const alignment = document.querySelector('d2l-select-outcomes').shadowRoot.querySelector('d2l-alignment-update').shadowRoot;
+			return alignment.querySelector('d2l-input-checkbox').shadowRoot.querySelector('label > input[type="checkbox"]');
+		});
+		t.ctx.addButton = Selector(() => {
+			const alignments = document.querySelector('d2l-select-outcomes').shadowRoot.querySelector('d2l-alignment-update').shadowRoot;
+			return alignments.querySelector('d2l-button');
+		});
+		await t
+			.click(t.ctx.checkBox);
+	});
 
-test ('Add Button Available After Selecting First Outcome', async t => {// eslint-disable-line no-undef
+test ('Add Button Is Not Disabled', async t => {// eslint-disable-line no-undef
 	await t
-		.navigateTo('http://127.0.0.1:8081/components/d2l-activity-alignments/demo/')
-		.wait(3000);//because webcomponts don't do 'end events' correctly so we have to make sure it is loaded before we go looking for stuff in the shadowRoots
-	const checkBox = Selector(() => {
-		const alignment = document.querySelector('d2l-select-outcomes').shadowRoot.querySelector('d2l-alignment-update').shadowRoot;
-		return alignment.querySelector('d2l-input-checkbox').shadowRoot.querySelector('label > input[type="checkbox"]');
-	});
-	const addButton = await Selector(() => {
+		.expect(t.ctx.addButton.hasAttribute('disabled')).notOk();
+});
+
+test ('Add Button is Disabled After Canceling Selection', async t =>{// eslint-disable-line no-undef
+	const cancelButton = Selector(() => {
 		const alignments = document.querySelector('d2l-select-outcomes').shadowRoot.querySelector('d2l-alignment-update').shadowRoot;
-		return alignments.querySelector('d2l-button');
-	});
+		return alignments.querySelectorAll('d2l-button');
+	}).withText('Cancel');
+
 	await t
-		.click(checkBox)
-		.expect(addButton.hasAttribute('disabled')).notOk();
+		.click(cancelButton)
+		.expect(t.ctx.addButton.hasAttribute('disabled')).ok();
 });
