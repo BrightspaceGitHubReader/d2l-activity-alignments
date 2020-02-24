c63551feb6cbb5bfc9a19048b88413b0122d0f3c
Yuan Lu <yuan.lu@d2l.com>
2020-02-24 17:12:56 -0500

    Finished search algo

--- d2l-select-outcomes-hierarchical-list.js
+++ d2l-select-outcomes-hierarchical-list.js
@@ -10,6 +10,7 @@ import { Actions, Classes, Rels } from 'd2l-hypermedia-constants';
 import 'd2l-colors/d2l-colors.js';
 import 'd2l-button/d2l-button.js';
 import 'd2l-inputs/d2l-input-checkbox.js';
+import 'd2l-inputs/d2l-input-search.js';
 import 'd2l-alert/d2l-alert.js';
 import 'd2l-loading-spinner/d2l-loading-spinner.js';
 import 'd2l-polymer-siren-behaviors/siren-entity-loading.js';
@@ -76,11 +77,26 @@ $_documentContainer.innerHTML = /*html*/`<dom-module id="d2l-select-outcomes-hie
 				background-color: var(--d2l-color-celestine-plus-2);
 				color: var(--d2l-color-celestine);
 			}
+
+			.center {
+				text-align: center;
+			}
 		</style>
+		<d2l-input-search
+			label="Search"
+			placeholder="Search..."
+			on-d2l-input-search-searched="_onSearch"
+		>
+		</d2l-input-search>
+		<template is="dom-if" if="[[_isEmptySearchResult]]">
+			<div class="center">
+				<h3>No results found for '[[_searchText]]'</h3>
+			</div>
+		</template>
 		<siren-entity-loading href="[[href]]" token="[[token]]">
 			<div class="d2l-alignment-update-content">
 				<ul class="d2l-hierarchy-tree" tabindex="0">
-					<template is="dom-repeat" items="[[_subHierarchyItems]]">
+					<template is="dom-repeat" items="[[_displayedHierarchyItems]]">
 						<li tabindex="-1" role="option" aria-selected$="[[_getAriaChecked(item)]]" aria-checked$="[[_getAriaChecked(item)]]">
 							<d2l-outcome-hierarchy-item id$="[[id]]" item="[[item]]" alignments="[[alignments]]"></d2l-outcome-hierarchy-item>
 						</li>
@@ -108,9 +124,25 @@ Polymer({
 		alignments: {
 			type: Set
 		},
-		_subHierarchyItems: {
+
+		_displayedHierarchyItems: {
 			type: Array,
-			computed: '_getHierarchy(entity)'
+			computed: '_getDisplayedHierarchyItems(_hierarchyItems, _searchText)'
+		},
+
+		_hierarchyItems: {
+			type: Array,
+			computed: '_getHierarchyItems(entity)'
+		},
+
+		_searchText: {
+			type: String
+		},
+
+		_isEmptySearchResult: {
+			type: Boolean,
+			value: false,
+			computed: '_getIsEmptySearchResult(_displayedHierarchyItems)'
 		}
 	},
 
@@ -120,13 +152,66 @@ Polymer({
 		}
 		return 'false';
 	},
-	
+
 	_getChecked: function(candidate) {
 		return true;
 	},
 
-	_getHierarchy: function(entity) {
+	_getHierarchyItems: function(entity) {
 		if (!entity || !entity.hasSubEntityByClass('hierarchical-outcome')) return [];
 		return entity.getSubEntitiesByClass('hierarchical-outcome');
+	},
+
+	_isLeafOutcome: function(entity) {
+		return entity && entity.class.includes('leaf-outcome');
+	},
+
+	_filterHierachy: function(item, searchText) {
+		if (Array.isArray(item)) {
+			// Top level tree
+			const topLevels = [];
+			for (const i of item) {
+				const filtered = this._filterHierachy(i, searchText);
+				if (filtered) {
+					topLevels.push(filtered);
+				}
+			}
+			return topLevels;
+		} else if (this._isLeafOutcome(item)) {
+			const search = (item, searchText = '') => {
+				const searchTarget = (item && item.properties && item.properties.description)
+					? item.properties.description.toLowerCase().normalize()
+					: '';
+				const searchTextLower = searchText.trim().toLowerCase().normalize();
+				return searchTarget.indexOf(searchTextLower) > -1;
+			};
+			return search(item, searchText) ? item : null;
+		} else {
+			// subtrees
+			const filteredSublevels = [];
+			for (const i of item.entities) {
+				if (this._filterHierachy(i, searchText)) {
+					filteredSublevels.push(i);
+				}
+			}
+			item.entities = filteredSublevels;
+			return filteredSublevels.length !== 0 ? item : null;
+		}
+	},
+
+	_getDisplayedHierarchyItems: function(items, searchText) {
+		if (!items) return [];
+		if (!searchText) return items;
+
+		const copy = JSON.parse(JSON.stringify(items)); // we don't want to contaminate the source data
+		return this._filterHierachy(copy, searchText);
+	},
+
+	_getIsEmptySearchResult: function(items) {
+		return items && items.length === 0;
+	},
+
+	_onSearch: function(e) {
+		this._searchText = e.detail.value;
 	}
 });
